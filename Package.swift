ae442e5f977afe756c32fb016ffe210dd158e709
Ell Neal <elliot@emdentec.com>
2019-12-14 10:45:41 +0000

    Use exact versions for indexstore-db and SwiftPM

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50100.0")),
-        .package(url: "https://github.com/apple/indexstore-db.git", .branch("swift-5.1-branch")),
-        .package(url: "https://github.com/apple/swift-package-manager.git", .branch("master")),
+        .package(url: "https://github.com/apple/indexstore-db.git", .revision("5960ba789ea7983516c7d419ad73ae264fced8ed")),
+        .package(url: "https://github.com/apple/swift-package-manager.git", .revision("528da5147ca82164acbd4cecdc8e69322d96d3e2")),
         .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0"),
     ],
     targets: [
